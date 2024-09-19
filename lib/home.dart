02e400f458ca57074fbe6d062f9998cf37c4db18
punkushal <spycymagar@gmail.com>
2024-09-19 07:34:50 +0545

    Person increament and Decrement functionality added

--- lib/home.dart
+++ lib/home.dart
@@ -12,7 +12,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _formKey = GlobalKey<FormState>();
   final _amountController = TextEditingController();
-
+  final double amount = 0.0;
+  int person = 0;
   @override
   void dispose() {
     _amountController.dispose();
@@ -57,7 +58,7 @@ class _HomeState extends State<Home> {
                       height: 8,
                     ),
                     Text(
-                      'Rs 450',
+                      'Rs $amount',
                       style: theme.textTheme.copyWith().titleLarge,
                     ),
                   ],
@@ -65,31 +66,87 @@ class _HomeState extends State<Home> {
               ),
             ),
 
-            // Input field for amount
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
-              child: TextFormField(
-                controller: _amountController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: "Enter amount",
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.grey, width: 0.5),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Input field for amount
+                  TextFormField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: "Enter amount",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.grey, width: 0.5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(224, 70, 63, 63),
+                            width: 0.5),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(255, 231, 88, 88),
+                            width: 0.5),
+                      ),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                        color: const Color.fromARGB(224, 70, 63, 63),
-                        width: 0.5),
+
+                  SizedBox(
+                    height: 38,
                   ),
-                  errorBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                        color: const Color.fromARGB(255, 231, 88, 88),
-                        width: 0.5),
+
+                  // Splitting amount to within total number of people
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Split',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              if (person <= 0) {
+                                ScaffoldMessenger.of(context).clearSnackBars();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    duration: Duration(seconds: 3),
+                                    content:
+                                        Text("Person number can't be negative"),
+                                  ),
+                                );
+                              } else {
+                                setState(() {
+                                  person = person - 1;
+                                });
+                              }
+                            },
+                            child: Icon(Icons.remove),
+                          ),
+                          Text("$person"),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                person++;
+                              });
+                            },
+                            child: Icon(Icons.add),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
-                ),
+                ],
               ),
             ),
           ],
